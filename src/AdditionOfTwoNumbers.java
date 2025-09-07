e1f24c1f8fd061695c4ee6dfbd964b3eb79c9f68
MadduruSivakumar <madduri.siva364@gmail.com>
2025-09-07 13:20:24 +0530

    Second Commit

---
+++ src/AdditionOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AdditionOfTwoNumbers {
+
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int c= a+b;
+
+        System.out.println("Addition of two numbers are:"+c);
+
+    }
+
+}
