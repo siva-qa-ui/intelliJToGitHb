3bd5439b1d54c1deb0b8ae74218e3df34feaaa09
MadduruSivakumar <madduri.siva364@gmail.com>
2025-09-16 06:42:29 +0530

    Literals and operators are added

---
+++ src/ex_02_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,8 @@
+package ex_02_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+    public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+    }
+}
