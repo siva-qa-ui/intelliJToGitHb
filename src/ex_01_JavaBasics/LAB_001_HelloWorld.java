2dd38d939791cf20d06da27f8826657c295c545f
MadduruSivakumar <madduri.siva364@gmail.com>
2025-09-16 06:27:30 +0530

    Ninith Table assignemnt

--- src/ex_01_JavaBasics/LAB_001_HelloWorld.java
+++ src/ex_01_JavaBasics/LAB_001_HelloWorld.java
@@ -4,11 +4,7 @@ public class LAB_001_HelloWorld
 {
     public static void main(String[] args)
     {
-
-        // Compute and print an arithmetic expression:
-        // 10 + (3 ÷ 2) − (1 × 5)
-
-        System.out.println(10 + 3 / 2 - 1 * 5);
+        System.out.println((10 + (3 / 2)) - (5));
 
     }
 
---
+++ src/ex_01_JavaBasics/NinthTable_Assignemnt.java
@@ -0,0 +1,19 @@
+package ex_01_JavaBasics;
+
+public class NinthTable_Assignemnt {
+    public static void main(String[] args) {
+        int table =9;
+        System.out.println("Multiplication of 9th table by using printf");
+        System.out.printf("%d X1 = %d%n", table, table * 1);
+        System.out.printf("%d X2 = %d%n", table, table * 2);
+        System.out.printf("%d X3 = %d%n", table, table * 3);
+        System.out.printf("%d X4 = %d%n", table, table * 4);
+        System.out.printf("%d X5 = %d%n", table, table * 5);
+        System.out.printf("%d X6 = %d%n", table, table * 6);
+        System.out.printf("%d X7 = %d%n", table, table * 7);
+        System.out.printf("%d X8 = %d%n", table, table * 8);
+        System.out.printf("%d X9 = %d%n", table, table * 9);
+        System.out.printf("%d X10 = %d%n", table, table * 10);
+
+    }
+}
